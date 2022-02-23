41255fbb505a2e6ee163cf2929bc5de0945e6d6d
kentguo_1997 <guoyunkai201709@gmail.com>
2022-02-23 18:31:44 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+// Build userSchema based on register page
+
+// Include mongoose
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+// setting Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }, 
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true  
+  }, 
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+// export
+module.exports = mongoose.model('User', userSchema) 
