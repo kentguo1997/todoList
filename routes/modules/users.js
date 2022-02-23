e90ff77020fb4b6af56d4f43862f3d79267c4d60
kentguo_1997 <guoyunkai201709@gmail.com>
2022-02-23 17:27:32 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,6 +8,13 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 // export
 module.exports = router
