c8048c5bf470ea2dca1cffc8527ca252a272d077
kentguo_1997 <guoyunkai201709@gmail.com>
2022-02-25 16:11:31 +0800

    feat: add user authentication in todo and home routers

---
+++ middleware/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/users/login')
+  }
+}
+
+// 重點:
+// 1. 這個設定檔會匯出一個物件，物件裡是一個叫做 authenticator 的函式。
+// 2. 其中的 req.isAuthenticated() 是 Passport.js 提供的函式，會根據 request 的登入狀態回傳 true 或 false。
+// 3. 如果 req.isAuthenticated() 回傳 true，則我們執行下一個 middleware，通常就會進入路由的核心功能，如果是 false，就強制返回 login 頁面。
--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,26 @@ const todos = require('./modules/todos')
 const users = require('./modules/users')
 
 
+// Include auth middleware for user authentication
+const { authenticator } = require('../middleware/auth')
+
+
+
 // Include routes
 
-// Homepage
-// 將網址結構符合 / 字串的 request 導向 home 模組, 執行modules/home裡面的程式碼 
-router.use('/', home)
 // 將網址結構符合 /todos 字串的 request 導向 todos 模組, 執行modules/todo裡面的程式碼 
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
+
 // 將網址結構符合 /users 字串的 request 導向 users 模組, 執行modules/users裡面的程式碼 
 router.use('/users', users)
+
+// 將網址結構符合 / 字串的 request 導向 home 模組, 執行modules/home裡面的程式碼 
+router.use('/', authenticator, home)
+
+// 重點:
+// 記得要把 router.use('/') 這種定義寬鬆的路由引到清單最下方，避免攔截到其他的路由。
+
+
 
 // Export routes for controller use
 module.exports = router
